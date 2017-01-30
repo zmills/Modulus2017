1508304997f3de939978cc1bbca06027c8613924
Zachary Mills <Zachary Mills@LAPTOP-7L17Q294>
2017-01-30 15:36:32 -0600

    Add project files.

---
+++ EaglesNestMobileApp.Android/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace EaglesNestMobileApp.Android
+{
+    [Activity(Label = "EaglesNestMobileApp.Android", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
