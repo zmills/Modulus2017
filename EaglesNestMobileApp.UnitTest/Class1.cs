6e2337874318f6cd5d5d1e69c0581292a3dc9925
Zachary Mills <itszmills@gmail.com>
2017-01-30 15:47:15 -0600

    test

--- EaglesNestMobileApp.UnitTest/Class1.cs
+++ EaglesNestMobileApp.UnitTest/Class1.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace EaglesNestMobileApp.UnitTest
+﻿namespace EaglesNestMobileApp.UnitTest
 {
     public class Class1
     {
