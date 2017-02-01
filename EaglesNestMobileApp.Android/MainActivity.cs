4d831cf2c2ecfb7597b55cc6a4d7a4e6bfa5bad0
Nicholas Baltodano <nbalto2567@students.pcci.edu>
2017-02-01 15:34:05 -0600

    Added Log in Screen
    Just the screen, will add the log feature next

--- EaglesNestMobileApp.Android/MainActivity.cs
+++ EaglesNestMobileApp.Android/MainActivity.cs
@@ -1,19 +1,25 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using System;
 
 namespace EaglesNestMobileApp.Android
 {
-    [Activity(Label = "EaglesNestMobileApp.Android", MainLauncher = true, Icon = "@drawable/icon")]
+    [Activity(Label = "", MainLauncher = true, Icon = "@drawable/logo")]
     public class MainActivity : Activity
     {
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
-
+          
             // Set our view from the "main" layout resource
-            // SetContentView (Resource.Layout.Main);
+            SetContentView(Resource.Layout.Main);
+
+            //Hide action bar
+            ActionBar.Hide();
         }
+
+      
     }
 }
 
--- EaglesNestMobileApp.Android/Resources/Resource.Designer.cs
+++ EaglesNestMobileApp.Android/Resources/Resource.Designer.cs
@@ -41,11 +41,45 @@ namespace EaglesNestMobileApp.Android
 			}
 		}
 		
+		public partial class Color
+		{
+			
+			// aapt resource value: 0x7f040003
+			public const int beige = 2130968579;
+			
+			// aapt resource value: 0x7f040005
+			public const int black = 2130968581;
+			
+			// aapt resource value: 0x7f040002
+			public const int colorAccent = 2130968578;
+			
+			// aapt resource value: 0x7f040000
+			public const int colorPrimary = 2130968576;
+			
+			// aapt resource value: 0x7f040001
+			public const int colorPrimaryDark = 2130968577;
+			
+			// aapt resource value: 0x7f040004
+			public const int eagleBlue = 2130968580;
+			
+			static Color()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Color()
+			{
+			}
+		}
+		
 		public partial class Drawable
 		{
 			
 			// aapt resource value: 0x7f020000
 			public const int Icon = 2130837504;
+			
+			// aapt resource value: 0x7f020001
+			public const int logo = 2130837505;
 			
 			static Drawable()
 			{
@@ -53,6 +87,31 @@ namespace EaglesNestMobileApp.Android
 			}
 			
 			private Drawable()
+			{
+			}
+		}
+		
+		public partial class Id
+		{
+			
+			// aapt resource value: 0x7f060003
+			public const int LogInButton = 2131099651;
+			
+			// aapt resource value: 0x7f060001
+			public const int inputUserId = 2131099649;
+			
+			// aapt resource value: 0x7f060002
+			public const int inputUserPassword = 2131099650;
+			
+			// aapt resource value: 0x7f060000
+			public const int logInImage = 2131099648;
+			
+			static Id()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Id()
 			{
 			}
 		}
@@ -76,11 +135,11 @@ namespace EaglesNestMobileApp.Android
 		public partial class String
 		{
 			
-			// aapt resource value: 0x7f040001
-			public const int ApplicationName = 2130968577;
+			// aapt resource value: 0x7f050001
+			public const int ApplicationName = 2131034113;
 			
-			// aapt resource value: 0x7f040000
-			public const int Hello = 2130968576;
+			// aapt resource value: 0x7f050000
+			public const int Hello = 2131034112;
 			
 			static String()
 			{
