efdd4cacc2270f789196d2f7f463708a0383d87d
zmills <itszmills@gmail.com>
2017-02-11 15:56:51 -0600

    MvvmLight + login activity

--- EaglesNestMobileApp.Android/Views/loginActivity.cs
+++ EaglesNestMobileApp.Android/Views/loginActivity.cs
@@ -1,19 +1,17 @@
 ﻿using Android.App;
-using Android.Widget;
 using Android.OS;
-using System;
-using Android.Content;
+using Android.Widget;
 using EaglesNestMobileApp.Android.Resources;
+using System;
 
 namespace EaglesNestMobileApp.Android
 {
-    [Activity(Label = "Eaglenest", MainLauncher = true, Icon = "@drawable/logo")]
-    public class MainActivity : Activity
+    [Activity(Label = "Eaglesnest", MainLauncher = true, Icon = "@drawable/logo")]
+    public class loginActivity : Activity
     {
         //private EditText username,
         //                 password;
         private Button button;
-
 
         protected override void OnCreate(Bundle bundle)
         {
@@ -35,21 +33,13 @@ namespace EaglesNestMobileApp.Android
             button = (Button)FindViewById(Resource.Id.LogInButton);
 
             button.Click += attempt_logIn;
-           
-
         }
 
-       
-        
+        /* Temporary method to show another layout                          */
         void attempt_logIn(object sender, EventArgs e)
         {
-
             //// Toast.MakeText(this, "No internet Baldy", ToastLength.Long).Show();
-     
             StartActivity(typeof(announcements));
-     
         }
-
-     
     }
 }
