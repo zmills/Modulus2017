84006c94206eef7f1d7915203a677cf2a7792355
zmills <itszmills@gmail.com>
2017-02-11 16:23:24 -0600

    Added some new folders and classes. Need to refactor the loginActivity to work with the loginActivityViewModel.

--- EaglesNestMobileApp.Android/Views/loginActivity.cs
+++ EaglesNestMobileApp.Android/Views/loginActivity.cs
@@ -9,8 +9,8 @@ namespace EaglesNestMobileApp.Android
     [Activity(Label = "Eaglesnest", MainLauncher = true, Icon = "@drawable/logo")]
     public class loginActivity : Activity
     {
-        //private EditText username,
-        //                 password;
+        private EditText username;
+                         //password;
         private Button button;
 
         protected override void OnCreate(Bundle bundle)
@@ -19,7 +19,7 @@ namespace EaglesNestMobileApp.Android
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
-            //username = (EditText)FindViewById(Resource.Id.inputUserId);
+            username = (EditText)FindViewById(Resource.Id.inputUserId);
 
             //Hide action bar
             var toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
@@ -38,7 +38,7 @@ namespace EaglesNestMobileApp.Android
         /* Temporary method to show another layout                          */
         void attempt_logIn(object sender, EventArgs e)
         {
-            //// Toast.MakeText(this, "No internet Baldy", ToastLength.Long).Show();
+            Toast.MakeText(this, "No internet Baldy", ToastLength.Long).Show();
             StartActivity(typeof(announcements));
         }
     }
---
+++ EaglesNestMobileApp.Core/ViewModel/loginActivityViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace EaglesNestMobileApp.Core.ViewModel
+{
+    public class loginActivityViewModel
+    {
+        // Handle all events/logic for the loginActivity here.
+    }
+}
