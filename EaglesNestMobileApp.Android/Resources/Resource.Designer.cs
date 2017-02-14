7bc01945df43075a9a3a78118998c5c560dc1ee1
Fabricevladimir <fabricevladimir@gmail.com>
2017-02-14 16:12:50 -0600

    Renamed bottom navigation

--- EaglesNestMobileApp.Android/Resources/Resource.Designer.cs
+++ EaglesNestMobileApp.Android/Resources/Resource.Designer.cs
@@ -2092,26 +2092,26 @@ namespace EaglesNestMobileApp.Android
 			// aapt resource value: 0x7f020053
 			public const int avd_hide_password = 2130837587;
 			
-			// aapt resource value: 0x7f02006a
-			public const int avd_hide_password_1 = 2130837610;
+			// aapt resource value: 0x7f02006f
+			public const int avd_hide_password_1 = 2130837615;
 			
-			// aapt resource value: 0x7f02006b
-			public const int avd_hide_password_2 = 2130837611;
+			// aapt resource value: 0x7f020070
+			public const int avd_hide_password_2 = 2130837616;
 			
-			// aapt resource value: 0x7f02006c
-			public const int avd_hide_password_3 = 2130837612;
+			// aapt resource value: 0x7f020071
+			public const int avd_hide_password_3 = 2130837617;
 			
 			// aapt resource value: 0x7f020054
 			public const int avd_show_password = 2130837588;
 			
-			// aapt resource value: 0x7f02006d
-			public const int avd_show_password_1 = 2130837613;
+			// aapt resource value: 0x7f020072
+			public const int avd_show_password_1 = 2130837618;
 			
-			// aapt resource value: 0x7f02006e
-			public const int avd_show_password_2 = 2130837614;
+			// aapt resource value: 0x7f020073
+			public const int avd_show_password_2 = 2130837619;
 			
-			// aapt resource value: 0x7f02006f
-			public const int avd_show_password_3 = 2130837615;
+			// aapt resource value: 0x7f020074
+			public const int avd_show_password_3 = 2130837620;
 			
 			// aapt resource value: 0x7f020055
 			public const int design_bottom_navigation_item_background = 2130837589;
@@ -2132,49 +2132,64 @@ namespace EaglesNestMobileApp.Android
 			public const int design_snackbar_background = 2130837594;
 			
 			// aapt resource value: 0x7f02005b
-			public const int Icon = 2130837595;
+			public const int ic_account = 2130837595;
 			
 			// aapt resource value: 0x7f02005c
-			public const int logo = 2130837596;
+			public const int ic_campus = 2130837596;
 			
 			// aapt resource value: 0x7f02005d
-			public const int navigation_empty_icon = 2130837597;
+			public const int ic_dining = 2130837597;
 			
 			// aapt resource value: 0x7f02005e
-			public const int notification_action_background = 2130837598;
+			public const int ic_grades = 2130837598;
 			
 			// aapt resource value: 0x7f02005f
-			public const int notification_bg = 2130837599;
+			public const int ic_home = 2130837599;
 			
 			// aapt resource value: 0x7f020060
-			public const int notification_bg_low = 2130837600;
+			public const int Icon = 2130837600;
 			
 			// aapt resource value: 0x7f020061
-			public const int notification_bg_low_normal = 2130837601;
+			public const int logo = 2130837601;
 			
 			// aapt resource value: 0x7f020062
-			public const int notification_bg_low_pressed = 2130837602;
+			public const int navigation_empty_icon = 2130837602;
 			
 			// aapt resource value: 0x7f020063
-			public const int notification_bg_normal = 2130837603;
+			public const int notification_action_background = 2130837603;
 			
 			// aapt resource value: 0x7f020064
-			public const int notification_bg_normal_pressed = 2130837604;
+			public const int notification_bg = 2130837604;
 			
 			// aapt resource value: 0x7f020065
-			public const int notification_icon_background = 2130837605;
-			
-			// aapt resource value: 0x7f020068
-			public const int notification_template_icon_bg = 2130837608;
-			
-			// aapt resource value: 0x7f020069
-			public const int notification_template_icon_low_bg = 2130837609;
+			public const int notification_bg_low = 2130837605;
 			
 			// aapt resource value: 0x7f020066
-			public const int notification_tile_bg = 2130837606;
+			public const int notification_bg_low_normal = 2130837606;
 			
 			// aapt resource value: 0x7f020067
-			public const int notify_panel_notification_icon_bg = 2130837607;
+			public const int notification_bg_low_pressed = 2130837607;
+			
+			// aapt resource value: 0x7f020068
+			public const int notification_bg_normal = 2130837608;
+			
+			// aapt resource value: 0x7f020069
+			public const int notification_bg_normal_pressed = 2130837609;
+			
+			// aapt resource value: 0x7f02006a
+			public const int notification_icon_background = 2130837610;
+			
+			// aapt resource value: 0x7f02006d
+			public const int notification_template_icon_bg = 2130837613;
+			
+			// aapt resource value: 0x7f02006e
+			public const int notification_template_icon_low_bg = 2130837614;
+			
+			// aapt resource value: 0x7f02006b
+			public const int notification_tile_bg = 2130837611;
+			
+			// aapt resource value: 0x7f02006c
+			public const int notify_panel_notification_icon_bg = 2130837612;
 			
 			static Drawable()
 			{
@@ -2188,6 +2203,21 @@ namespace EaglesNestMobileApp.Android
 		
 		public partial class Id
 		{
+			
+			// aapt resource value: 0x7f0a00a2
+			public const int BottomNavIconAccount = 2131361954;
+			
+			// aapt resource value: 0x7f0a00a0
+			public const int BottomNavIconCampus = 2131361952;
+			
+			// aapt resource value: 0x7f0a00a1
+			public const int BottomNavIconDining = 2131361953;
+			
+			// aapt resource value: 0x7f0a009f
+			public const int BottomNavIconGrades = 2131361951;
+			
+			// aapt resource value: 0x7f0a009e
+			public const int BottomNavIconHome = 2131361950;
 			
 			// aapt resource value: 0x7f0a0084
 			public const int LogInButton = 2131361924;
@@ -2431,6 +2461,12 @@ namespace EaglesNestMobileApp.Android
 			
 			// aapt resource value: 0x7f0a008b
 			public const int media_actions = 2131361931;
+			
+			// aapt resource value: 0x7f0a00a4
+			public const int menu_settings = 2131361956;
+			
+			// aapt resource value: 0x7f0a00a3
+			public const int menu_share = 2131361955;
 			
 			// aapt resource value: 0x7f0a003b
 			public const int middle = 2131361851;
@@ -2911,6 +2947,28 @@ namespace EaglesNestMobileApp.Android
 			}
 			
 			private Layout()
+			{
+			}
+		}
+		
+		public partial class Menu
+		{
+			
+			// aapt resource value: 0x7f0d0000
+			public const int bottom_nav_menu = 2131558400;
+			
+			// aapt resource value: 0x7f0d0001
+			public const int sample_home_menu = 2131558401;
+			
+			// aapt resource value: 0x7f0d0002
+			public const int sample_photo_edit_menu = 2131558402;
+			
+			static Menu()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Menu()
 			{
 			}
 		}
