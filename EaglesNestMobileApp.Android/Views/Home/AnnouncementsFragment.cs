39a0ac4ed025c0d82d7a2493c8f33522faf330c7
mpaugello <marcelino.augello7@gmail.com>
2017-02-20 03:11:04 -0500

    Experimenting with RecyclerView in AnnouncementsFragmentLayout, AnnouncementsFragment

--- EaglesNestMobileApp.Android/Views/Home/AnnouncementsFragment.cs
+++ EaglesNestMobileApp.Android/Views/Home/AnnouncementsFragment.cs
@@ -1,10 +1,16 @@
 using Android.OS;
 using Android.Views;
 using Android.Support.V4.App;
+using Android.Support.V7.Widget;
+
 namespace EaglesNestMobileApp.Android.Views.Home
 {
     public class announcementsFragment : Fragment
     {
+        private RecyclerView announceRecyclerView;
+        private RecyclerView.Adapter announceAdapter;
+        private RecyclerView.LayoutManager announceLayoutManager;
+
         public override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -14,6 +20,8 @@ namespace EaglesNestMobileApp.Android.Views.Home
 
         public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
         {
+            View announcementsView = inflater.Inflate(Resource.Layout.AnnouncementsFragmentLayout, container, false);
+
             // Use this to return your custom view for this Fragment
             return inflater.Inflate(Resource.Layout.AnnouncementsFragmentLayout, container, false);
         }
