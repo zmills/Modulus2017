04c1c8e14472dd63f00ea5effd434d22aa4b28c0
mpaugello <marcelino.augello7@gmail.com>
2017-02-24 20:05:32 -0500

    Grades Page Expandable Card

--- EaglesNestMobileApp.Android/Adapters/gradesViewHolder.cs
+++ EaglesNestMobileApp.Android/Adapters/gradesViewHolder.cs
@@ -10,11 +10,14 @@ namespace EaglesNestMobileApp.Android.Adapters
         // Public accessors
         // Instantiate a viewholder TextView to be set with a class name
         public TextView GradesClassName { get; set; }
+        public LinearLayout llExpandArea;
 
         public gradesViewHolder(View view) : base(view)
         {
             // Set the GradesClassName TextView to the GradesCardClassName textview in the layout
             GradesClassName = view.FindViewById<TextView>(Resource.Id.GradesCardClassName);
+
+            llExpandArea = view.FindViewById<LinearLayout>(Resource.Id.llExpandArea);
         }
 
         // Take in a gradesCard and assign its data to the viewholder's views
