2351af7d820964598de5e1fc601dff8fed1f8132
zmills <itszmills@gmail.com>
2017-02-28 20:45:40 -0600

    AzureToken comments.

--- EaglesNestMobileApp.Core/Model/AzureToken.cs
+++ EaglesNestMobileApp.Core/Model/AzureToken.cs
@@ -1,24 +1,24 @@
-﻿//*************************************************************************/
-//*                               AzureToken                              */
-//* This is the model corresponding to the view in the Azure SQL database */
-//* It is used for authenctication purposes.                              */
-//*                                                                       */
-//*************************************************************************/
+﻿/*******************************************************************************/
+/*                                 AzureToken                                  */
+/*    This is the model corresponding to the view in the Azure SQL database    */
+/*    It is used for authentication purposes.                                  */
+/*                                                                             */
+/*******************************************************************************/
 
 using System;
 
 namespace EaglesNestMobileApp.Core.Model
 {
-   public class AzureToken
-   {
-      public string Id { get; set; }
+    public class AzureToken
+    {
+        public string Id { get; set; }
 
-      public string Salt { get; set; }
-      public string Passwordhash { get; set; }
-      public DateTimeOffset CreatedAt { get; set; }
-      public DateTimeOffset UpdatedAt { get; set; }
-      public string Version { get; set; }
-      public bool Deleted { get; set; }
+        public string Salt { get; set; }
+        public string Passwordhash { get; set; }
+        public DateTimeOffset CreatedAt { get; set; }
+        public DateTimeOffset UpdatedAt { get; set; }
+        public string Version { get; set; }
+        public bool Deleted { get; set; }
 
-   }
+    }
 }
