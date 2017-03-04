a32bc420f1793bb7af32268649fe8a032e731c2c
Conrad <Conrad@DESKTOP-26C7AJM>
2017-03-04 15:04:40 -0600

    Changed the single comment in Class.cs to fit the Coding Standard.

--- EaglesNestMobileApp.Core/Model/Academics/Class.cs
+++ EaglesNestMobileApp.Core/Model/Academics/Class.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace EaglesNestMobileApp.Core.Model
 {
-    // This class corresponds to the ENROLLMENT TABLE in the database
+    /* This class corresponds to the ENROLLMENT TABLE in the database        */
     public class Class
     {
         public string Id { get; set; }
