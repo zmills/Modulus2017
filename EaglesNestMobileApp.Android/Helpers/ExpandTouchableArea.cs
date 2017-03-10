821253693305653d75123259c926f0b8abab09f2
mpaugello <marcelino.augello7@gmail.com>
2017-03-10 01:51:47 -0500

    comments...

--- EaglesNestMobileApp.Android/Helpers/ExpandTouchableArea.cs
+++ EaglesNestMobileApp.Android/Helpers/ExpandTouchableArea.cs
@@ -1,19 +1,19 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
 
-using Android.App;
-using Android.Content;
-using Android.OS;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
+//using Android.App;
+//using Android.Content;
+//using Android.OS;
+//using Android.Runtime;
+//using Android.Views;
+//using Android.Widget;
 
-namespace EaglesNestMobileApp.Android.Helpers
-{
-    public static class ExpandTouchableArea
-    {
+//namespace EaglesNestMobileApp.Android.Helpers
+//{
+//    public static class ExpandTouchableArea
+//    {
         
-    }
-}
+//    }
+//}
