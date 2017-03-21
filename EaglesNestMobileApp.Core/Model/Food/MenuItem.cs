a2e0538eaf8713c3d2bf6b1d17c05f42e0f2a395
Fabricevladimir <fabricevladimir@gmail.com>
2017-03-21 16:07:17 -0500

    Changed menu item, removed the v7 stuff

--- EaglesNestMobileApp.Core/Model/Food/MenuItem.cs
+++ EaglesNestMobileApp.Core/Model/Food/MenuItem.cs
@@ -1,9 +1,4 @@
-﻿using Android.Support.V7.Widget;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace EaglesNestMobileApp.Core.Model.Food
 {
