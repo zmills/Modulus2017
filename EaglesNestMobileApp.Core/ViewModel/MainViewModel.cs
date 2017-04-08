65d4e4009c410052fb21c13a0c5c52ac89c0335a
Fabricevladimir <fabricevladimir@gmail.com>
2017-04-07 20:45:17 -0500

    Dynamic data

--- EaglesNestMobileApp.Core/ViewModel/MainViewModel.cs
+++ EaglesNestMobileApp.Core/ViewModel/MainViewModel.cs
@@ -10,16 +10,16 @@ namespace EaglesNestMobileApp.Core.ViewModel
         public async Task InitializeViewModels()
         {
             App.Locator.Events.Initialize();
-            App.Locator.Grades.InitializeStatic();
-            //await App.Locator.Grades.InitializeAsync();
-            //await App.Locator.Exams.Initialize();
+            //App.Locator.Grades.InitializeStatic();
+            await App.Locator.Grades.InitializeAsync();
+            await App.Locator.Exams.Initialize();
             App.Locator.Exams.InitializeStatic();
-            //await App.Locator.GrabAndGo.InitializeAsync();
-            App.Locator.GrabAndGo.InitializeVm();
-            //await App.Locator.StudentInfo.InitializeAsync();
-            //await App.Locator.Varsity.InitializeAsync();
+            await App.Locator.GrabAndGo.InitializeAsync();
+            //App.Locator.GrabAndGo.InitializeVm();
+            await App.Locator.StudentInfo.InitializeAsync();
+            await App.Locator.Varsity.InitializeAsync();
             App.Locator.Varsity.InitializeStatic();
-            //await App.Locator.FourWinds.InitializeAsync();
+            await App.Locator.FourWinds.InitializeAsync();
             App.Locator.FourWinds.InitializeStatic();
             App.Locator.Attendance.InitializeStatic();
             App.Locator.StudentSchedule.InitializeStatic();
